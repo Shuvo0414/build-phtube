209dbe474e0578eba88763b4a0fa9cb6ab0a26b2
Shuvo0414 <138393384+Shuvo0414@users.noreply.github.com>
2023-08-31 20:26:26 +0600

    get the category data

--- js/index.js
+++ js/index.js
@@ -1,1 +1,21 @@
 // console.log("all okh");
+const loadData = async () => {
+  const res = await fetch(
+    "https://openapi.programming-hero.com/api/videos/categories"
+  );
+  const data = await res.json();
+  const phTubeData = data.data;
+  //   console.log(phTubeData);
+  const tabContainer = document.querySelector("#tab-container");
+  phTubeData.forEach((category) => {
+    console.log(category);
+    const div = document.createElement("div");
+    div.innerHTML = `
+    <a class="tab text-base font-medium w-[104px] bg-[#25252526] rounded"
+          >${category.category}</a
+        >
+    `;
+    tabContainer.appendChild(div);
+  });
+};
+loadData();
